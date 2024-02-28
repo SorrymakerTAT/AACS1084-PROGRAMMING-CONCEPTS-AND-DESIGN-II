7782a7103f98dc89c1569e5f1023ef0f8599e278
HarrisonQAQ <92703206+SorrymakerTAT@users.noreply.github.com>
2024-02-28 14:14:00 +0800

    P1Q3.c

--- P1Q3.c
+++ P1Q3.c
@@ -1,43 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
-#pragma warning(disable:4996)
+#pragma warning (disable:4996)
 
 struct Time {
 	int hour;
 	int minute;
+
 };
 
-void main()
-{
 
-	struct Time current, nextMin;
-	current.hour;
-	current.minute;
-	nextMin.hour;
-	nextMin.minute;
+void main() {
+	struct Time current;
+	struct Time nextMin;
 
-	printf("Please Enter Your Current Time in hours and minuted :");
-	scanf("%d:%02d", &current.hour, &current.minute);
+	printf("Input current hour :");
+	scanf("%d", &current.hour);
+	rewind(stdin);
+	printf("Input current minute :");
+	scanf("%d", &current.minute);
 	rewind(stdin);
 
+	nextMin.minute = 0;
+	nextMin.hour = 0;
+
 	nextMin.minute = current.minute + 1;
-	if (nextMin.minute >= 60)
-	{
-		nextMin.hour = current.hour + 1;
-		if (nextMin.hour > 12) {
-			nextMin.hour = 1;
-		}
-		else
-		{
-			nextMin.hour = nextMin.hour;
-		}
-		nextMin.minute = 00;
-	}
-	else
-	{
-		nextMin.hour = current.hour;
+	nextMin.hour = current.hour; 
+
+	if (nextMin.minute >= 60) {
+		nextMin.hour += 1;
+		nextMin.minute -= 60;
 	}
 
-	printf("It is now %02d:%02d, Next minute it will be %d:%02d\n", current.hour, current.minute, nextMin.hour, nextMin.minute);
-	system("pause");
+	if (nextMin.hour >= 13 && nextMin.hour <= 23) {
+		nextMin.hour -= 12;
+	}
+
+	printf("\nIt is now %d:%02d. Next minute it will be %d:%02d",current.hour,current.minute,nextMin.hour,nextMin.minute);
 }
