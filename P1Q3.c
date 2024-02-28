b9e95582b5c91563ed7cf63fd1f9fb8fbbe75d3b
HarrisonQAQ <92703206+SorrymakerTAT@users.noreply.github.com>
2024-02-28 13:31:10 +0800

    P1Q3.c

--- P1Q3.c
+++ P1Q3.c
@@ -3,39 +3,41 @@
 #pragma warning(disable:4996)
 
 struct Time {
-	int hour, minute;
+	int hour;
+	int minute;
 };
 
-void mian() {
+void main()
+{
+
 	struct Time current, nextMin;
+	current.hour;
+	current.minute;
+	nextMin.hour;
+	nextMin.minute;
 
-	printf("Enter time : \n");
-	printf("\tHour : "); //13
-	scanf("%d", &current.hour);
-	printf("\tMinute : "); //62
-	scanf("%d", &current.minute);
+	printf("Please Enter Your Current Time in hours and minuted :");
+	scanf("%d:%02d", &current.hour, &current.minute);
+	rewind(stdin);
 
-	while(current.hour < 0 || current.hour > 12 || current.minute < 0 || current.minute >= 60)
-	{
-		printf("Enter time : \n");
-		printf("\tHour : "); 
-		scanf("%d", &current.hour);//12
-		printf("\tMinute : "); 
-		scanf("%d", &current.minute);//59
-	}
-
-	nextMin.hour = current.hour;
 	nextMin.minute = current.minute + 1;
-
 	if (nextMin.minute >= 60)
 	{
-		nextMin.minute >= 60;
 		nextMin.hour = current.hour + 1;
-		if (nextMin.hour > 12)
-			nextMin.hour -= 12;
+		if (nextMin.hour > 12) {
+			nextMin.hour = 1;
+		}
+		else
+		{
+			nextMin.hour = nextMin.hour;
+		}
+		nextMin.minute = 00;
+	}
+	else
+	{
+		nextMin.hour = current.hour;
 	}
 
-	printf("\nIt is now %02d:%02d.Next minute it will be %02d:%02d\n",current.hour, current.minute, nextMin.hour, nextMin.minute);
-
+	printf("It is now %02d:%02d, Next minute it will be %d:%02d\n", current.hour, current.minute, nextMin.hour, nextMin.minute);
 	system("pause");
 }
